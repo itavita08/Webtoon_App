39c955cf5d67d52d34a1cfda86939cafdd0a432e
itavita08 <dudwns10000@gmail.com>
2023-03-16 11:02:42 +0900

    오늘의 웹툰 api에서 데이터 추출

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // List<WebtoonModel> webtoons = [];
+  List<dynamic> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    for (Map<String, dynamic> i in webtoons) {
+      print(i['title']);
+    }
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,19 +4,22 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/model/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  // static Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<dynamic>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse("$baseUrl/$today");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
-      for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
-      }
-      return webtoonInstances;
+      // for (var webtoon in webtoons) {
+      //   webtoonInstances.add(WebtoonModel(webtoon));
+      // }
+      // return webtoonInstances;
+      return webtoons;
     }
     throw Error();
   }
