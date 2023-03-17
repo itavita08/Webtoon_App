f3de0266258f6e1a0d34d362600416ef2b8d77ad
itavita08 <dudwns10000@gmail.com>
2023-03-18 02:32:40 +0900

    detail, episode api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/model/webtoon_detail_model.dart';
+import 'package:toonflix/model/webtoon_episode_model.dart';
 import 'package:toonflix/model/webtoon_model.dart';
 
 class ApiService {
@@ -20,6 +22,31 @@ class ApiService {
       }
       return webtoonInstances;
       // return webtoons;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatesEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
