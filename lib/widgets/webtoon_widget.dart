9a0c7a74d22b3857c5dc4f18acf4e12172c1d2dd
itavita08 <dudwns10000@gmail.com>
2023-03-18 00:34:46 +0900

    hero widget 사용

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -18,6 +18,7 @@ class WEbtoon extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
+            // PadgeROuteBuilder : 더 다양한 애니메이션 적용 가능,
             builder: (context) =>
                 DetailScreen(title: title, thumb: thumb, id: id),
             fullscreenDialog: true,
@@ -26,26 +27,29 @@ class WEbtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip
-                .hardEdge, // radius를 적용해도 이미지가 가려버리기 때문에 이미지를 부모인 container에 넘어가지 않게 하기위해
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                )
-              ],
-            ),
-            child: Image.network(
-              thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip
+                  .hardEdge, // radius를 적용해도 이미지가 가려버리기 때문에 이미지를 부모인 container에 넘어가지 않게 하기위해
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
